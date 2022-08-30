c1dcf12fbe4525d91697e71f95852e4a51c9f100
Seyyed Hesam Rajaei <h.rajaei@phinix.com>
2022-08-30 13:15:32 +0100

    fix: docker file

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -5,6 +5,6 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'ar'],
   },
-
+  react: { useSuspense: false },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
+  experimental: {
+    outputStandalone: true,
+  },
   reactStrictMode: true,
   i18n,
 };
