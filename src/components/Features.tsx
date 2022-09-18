82a780f2eb7398c98f72742a71e25534a6e04b07
Seyyed Hesam Rajaei <h.rajaei@phinix.com>
2022-09-18 21:03:08 +0100

    refactor: features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,9 +6,9 @@ export const Features = () => {
   const { t } = useTranslation();
 
   const tabs = [
-    { id: 'P2P', label: 'P2P' },
-    { id: 'OTC', label: 'OTC' },
-    { id: 'Express', label: 'Express' },
+    { id: 'P2P', label: t('P2P.name'), isDisable: false },
+    { id: 'OTC', label: t('OTC.name'), isDisable: true },
+    { id: 'Express', label: t('Express.name'), isDisable: true },
   ];
   const [tab, setTab] = useState(tabs[0].id);
 
@@ -17,7 +17,8 @@ export const Features = () => {
       <Text
         size={32}
         weight="bold"
-        css={{ pt: '$20', mb: '$8', '@xsMax': { fs: 24 } }}>
+        css={{ pt: '$20', mb: '$8', '@xsMax': { fs: 24 } }}
+      >
         {t('featuresFull')}
       </Text>
 
@@ -29,15 +30,18 @@ export const Features = () => {
         <Grid
           xs={12}
           sm={10}
-          css={{ minHeight: 410, '@smMax': { minHeight: 600 } }}>
-          {tab === 'P2P' && <P2P />}
+          css={{ minHeight: 410, '@smMax': { minHeight: 600 } }}
+        >
+          {tab === 'P2P' && <FeatureDetails featureName="P2P" />}
+          {tab === 'OTC' && <FeatureDetails featureName="OTC" />}
+          {tab === 'Express' && <FeatureDetails featureName="Express" />}
         </Grid>
       </Grid.Container>
     </section>
   );
 };
 
-type TabType = { id: string; label: string };
+type TabType = { id: string; label: string; isDisable: boolean };
 
 const Tabs = ({
   tabs,
@@ -57,13 +61,19 @@ const Tabs = ({
         justifyContent: 'center',
         borderBottom: '2px solid',
         borderBottomColor: activeTab === tab.id ? '$primary' : '#BBBBBB',
-      }}>
-      <Button onPress={() => setTab(tab.id)} css={{ w: '100%' }}>
+      }}
+    >
+      <Button
+        disabled={tab.isDisable}
+        onPress={() => setTab(tab.id)}
+        css={{ w: '100%' }}
+      >
         <Text
           size={18}
           weight="bold"
           css={{ '@xsMax': { fs: 14 } }}
-          color={activeTab === tab.id ? '$primary' : '#BBBBBB'}>
+          color={activeTab === tab.id ? '$primary' : '#BBBBBB'}
+        >
           {tab.label}
         </Text>
       </Button>
@@ -78,149 +88,146 @@ const Tabs = ({
         w: '100%',
         m: '40px 0',
         justifyContent: 'space-between',
-      }}>
+      }}
+    >
       {tabs.map(TabItem)}
     </Button.Group>
   );
 };
 
-const P2P = () => (
-  <Grid.Container>
-    <Grid xs={12}>
-      <Text css={{ mb: '$12' }}>
-        Buy and sell USDT(Tether) safely and easily on Nakhlex P2P Trade
-        platform. Find the best offer and buy and sell USDT with Your Preferred
-        Payment Methods.
-      </Text>
-    </Grid>
+const FeatureDetails = ({ featureName }: { featureName: string }) => {
+  const { t } = useTranslation();
 
-    <Grid xs={12} sm={4} direction="column">
-      <Text size={18} weight="bold">
-        Advantages of P2P Trade
-      </Text>
+  return (
+    <Grid.Container>
+      <Grid xs={12}>
+        <Text css={{ mb: '$12' }}>{t(`${featureName}.description`)}</Text>
+      </Grid>
 
-      <ul style={{ paddingBottom: 16 }}>
-        <li style={{ paddingTop: 16 }}>Low cost transaction fees</li>
-        <li style={{ paddingTop: 16 }}>Flexible payment methods</li>
-        <li style={{ paddingTop: 16 }}>Trade at your preferred prices</li>
-        <li style={{ paddingTop: 16 }}>Protection for your privacy</li>
-      </ul>
-    </Grid>
+      <Grid xs={12} sm={4} direction="column">
+        <Text size={18} weight="bold">
+          {t(`${featureName}.advantages`)}
+        </Text>
 
-    <Grid xs={12} sm={8}>
-      <Grid.Container>
-        <Grid
-          css={{
-            bg: '#F8F8F8',
-            ta: 'center',
-            py: '$5',
-            borderRadius: '12px 12px 0 0',
-            border: '1px solid #fff',
-          }}
-          justify="center"
-          xs={12}>
-          <Text size={18} weight="bold">
-            How P2P Works
-          </Text>
-        </Grid>
+        <ul style={{ paddingBottom: 16 }}>
+          <li style={{ paddingTop: 16 }}>{t(`${featureName}.advantage01`)}</li>
+          <li style={{ paddingTop: 16 }}>{t(`${featureName}.advantage02`)}</li>
+          <li style={{ paddingTop: 16 }}>{t(`${featureName}.advantage03`)}</li>
+          <li style={{ paddingTop: 16 }}>{t(`${featureName}.advantage04`)}</li>
+        </ul>
+      </Grid>
 
-        <Grid
-          css={{
-            bg: '#F8F8F8',
-            ta: 'center',
-            py: '$5',
-            border: '1px solid #fff',
-          }}
-          justify="center"
-          xs={6}>
-          <Text size={16} weight="bold">
-            Buy Crypto
-          </Text>
-        </Grid>
+      <Grid xs={12} sm={8}>
+        <Grid.Container>
+          <Grid
+            css={{
+              bg: '#F8F8F8',
+              ta: 'center',
+              py: '$5',
+              borderRadius: '12px 12px 0 0',
+              border: '1px solid #fff',
+            }}
+            justify="center"
+            xs={12}
+          >
+            <Text size={18} weight="bold">
+              {t('howWorks', { feature: t(`${featureName}.name`) })}
+            </Text>
+          </Grid>
 
-        <Grid
-          css={{
-            bg: '#F8F8F8',
-            ta: 'center',
-            py: '$5',
-            border: '1px solid #fff',
-          }}
-          justify="center"
-          xs={6}>
-          <Text size={16} weight="bold">
-            Sell Crypto
-          </Text>
-        </Grid>
+          <Grid
+            css={{
+              bg: '#F8F8F8',
+              ta: 'center',
+              py: '$5',
+              border: '1px solid #fff',
+            }}
+            justify="center"
+            xs={6}
+          >
+            <Text size={16} weight="bold">
+              {t(`${featureName}.buy`)}
+            </Text>
+          </Grid>
 
-        <Grid
-          css={{
-            bg: '#F8F8F8',
-            p: '$8',
-            borderRadius: '0 0 0 12px',
-            border: '1px solid #fff',
-          }}
-          xs={6}
-          direction="column">
-          <Text size={14} weight="bold">
-            Plan an order
-          </Text>
-          <Text size={14} css={{ mt: '$3', mb: '$5' }}>
-            Once you place a P2P order, the crypto asset will be escrowed by
-            Nakhlex P2P.
-          </Text>
+          <Grid
+            css={{
+              bg: '#F8F8F8',
+              ta: 'center',
+              py: '$5',
+              border: '1px solid #fff',
+            }}
+            justify="center"
+            xs={6}
+          >
+            <Text size={16} weight="bold">
+              {t(`${featureName}.sell`)}
+            </Text>
+          </Grid>
 
-          <Text size={14} weight="bold">
-            Pay the Seller
-          </Text>
-          <Text size={14} css={{ mt: '$3', mb: '$5' }}>
-            Send money to the seller via the suggested payment methods. Complete
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
-            the fiat transaction and click "I've paid" on Nakhlex P2P.
-          </Text>
+          <Grid
+            css={{
+              bg: '#F8F8F8',
+              p: '$8',
+              borderRadius: '0 0 0 12px',
+              border: '1px solid #fff',
+            }}
+            xs={6}
+            direction="column"
+          >
+            <Text size={14} weight="bold">
+              {t(`${featureName}.buyItem01.title`)}
+            </Text>
+            <Text size={14} css={{ mt: '$3', mb: '$5' }}>
+              {t(`${featureName}.buyItem01.subtitle`)}
+            </Text>
 
-          <Text size={14} weight="bold">
-            Get your Crypto
-          </Text>
-          <Text size={14} css={{ mt: '$3', mb: '$5' }}>
-            Once the seller confirms receipt of money, the escrowed crypto will
-            be released to you.
-          </Text>
-        </Grid>
+            <Text size={14} weight="bold">
+              {t(`${featureName}.buyItem02.title`)}
+            </Text>
+            <Text size={14} css={{ mt: '$3', mb: '$5' }}>
+              {t(`${featureName}.buyItem02.subtitle`)}
+            </Text>
 
-        <Grid
-          css={{
-            bg: '#F8F8F8',
-            p: '$8',
-            borderRadius: '0 0 12px 0',
-            border: '1px solid #fff',
-          }}
-          xs={6}
-          direction="column">
-          <Text size={14} weight="bold">
-            Plan an order
-          </Text>
-          <Text size={14} css={{ mt: '$3', mb: '$5' }}>
-            After you place an order, your crypto will be escrowed by Nakhlex
-            P2P.
-          </Text>
+            <Text size={14} weight="bold">
+              {t(`${featureName}.buyItem03.title`)}
+            </Text>
+            <Text size={14} css={{ mt: '$3', mb: '$5' }}>
+              {t(`${featureName}.buyItem03.subtitle`)}
+            </Text>
+          </Grid>
 
-          <Text size={14} weight="bold">
-            Confirm the Payment
-          </Text>
-          <Text size={14} css={{ mt: '$3', mb: '$5' }}>
-            Check the transaction record in the given payment account, and make
-            sure you receive the money sent by the buyer.
-          </Text>
-
-          <Text size={14} weight="bold">
-            Release Crypto
-          </Text>
-          <Text size={14} css={{ mt: '$3', mb: '$5' }}>
-            Once you confirm the receipt of money, release crypto to the buyer
-            on Nakhlex P2P.
-          </Text>
-        </Grid>
-      </Grid.Container>
-    </Grid>
-  </Grid.Container>
-);
+          <Grid
+            css={{
+              bg: '#F8F8F8',
+              p: '$8',
+              borderRadius: '0 0 12px 0',
+              border: '1px solid #fff',
+            }}
+            xs={6}
+            direction="column"
+          >
+            <Text size={14} weight="bold">
+              {t(`${featureName}.sellItem01.title`)}
+            </Text>
+            <Text size={14} css={{ mt: '$3', mb: '$5' }}>
+              {t(`${featureName}.sellItem01.subtitle`)}
+            </Text>
+            <Text size={14} weight="bold">
+              {t(`${featureName}.sellItem02.title`)}
+            </Text>
+            <Text size={14} css={{ mt: '$3', mb: '$5' }}>
+              {t(`${featureName}.sellItem02.subtitle`)}
+            </Text>
+            <Text size={14} weight="bold">
+              {t(`${featureName}.sellItem03.title`)}
+            </Text>
+            <Text size={14} css={{ mt: '$3', mb: '$5' }}>
+              {t(`${featureName}.sellItem03.subtitle`)}
+            </Text>
+          </Grid>
+        </Grid.Container>
+      </Grid>
+    </Grid.Container>
+  );
+};
