e79a1c9bcae849237892d6f869e5822b06916da3
Hessam Rajaei <h.rajaei@wallex.net>
2022-11-12 16:10:11 +0000

    Update Market.tsx

--- src/components/Market.tsx
+++ src/components/Market.tsx
@@ -9,7 +9,6 @@ import socketIOClient from 'socket.io-client';
 import { Abbreviation, MarketType, PriceType } from '../types';
 
 const host = process.env.NEXT_PUBLIC_SOCKET_HOST;
-console.log('host: ', host);
 
 export const echo = new Echo({
   host,
